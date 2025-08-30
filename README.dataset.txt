940755153c0a6fb6ebae72eb8e09fe7ad76396b4
Baastheglass <lightningblazer123@gmail.com>
2025-08-30 23:29:26 +0500

    about to train on coco segmentation

---
+++ README.dataset.txt
@@ -0,0 +1,6 @@
+# caries > 2025-08-21 12:08am
+https://universe.roboflow.com/baasils-workspace/caries-laxcp-m6wtd
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ coco_try.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+
+# Load a pretrained YOLO11 instance segmentation model
+model = YOLO("yolo11n-seg.pt")  # can also use .yaml if building from scratch
+
+# Train the model
+results = model.train(
+    data="dataset/data.yaml",
+    epochs=100,
+    imgsz=640,
+    batch=8,       # adjust based on GPU memory
+    name="my_segmentation_experiment"
+)
+
+# Optionally, validate afterward
+metrics = model.val()
+print("Box mAP:", metrics.box.map, "| Mask mAP:", metrics.seg.map)
